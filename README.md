6a2c0e064e42dcc0a6ba4a9bc560839782f0a6eb
ADMSK\AVROGAL1 <avrogal1@mts.ru>
2021-01-17 13:50:05 +0300

    Init repo

---
+++ README.md
@@ -0,0 +1,19 @@
+Custom BottomNavigationView
+-------------------------------
+
+BottomNavigationView is for creating prevent icon shifting after implement example you can see your icon not shift
+Inside application i will create 4 static menu and click on every menu other icon not shift or move.
+
+If my example really helpful to you then please don't forgot to press `Star` icon.
+
+
+**Important Note :**
+------------------------
+Please don't use example bottom navigation icon please create your own icon and use.
+
+Screenshots
+-----------
+
+![screenshot][1]
+
+[1]: ./art/art.gif
---
+++ app/src/main/java/com/vp/bottomnavigationview/CategoryFragment.java
@@ -0,0 +1,31 @@
+package com.vp.bottomnavigationview;
+
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by Vishal Patolia on 18-Feb-18.
+ */
+
+public class CategoryFragment extends Fragment {
+
+    @BindView(R.id.home_txtTitle)
+    TextView mHomeTxtTitle;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+        ButterKnife.bind(this, rootView);
+        mHomeTxtTitle.setText(getString(R.string.menu_categories));
+        return rootView;
+    }
+}
